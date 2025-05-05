35864fa226d97ce8948981cb3eb5ee06a9b9c315
MakyJame <ngoctai1995@gmail.com>
2025-05-05 16:31:50 +0700

    Fix bugs and update UI

---
+++ DraftAboutReserveEngineer/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraftAboutReserveEngineer.Helpers
+{
+    public class ConfigurationHelper
+    {
+        public static IConfigurationRoot GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsetting.json", optional: false, reloadOnChange: true);
+            return builder.Build();
+        }
+    }
+}
--- DraftAboutReserveEngineer/Models/AppDbContext.cs
+++ DraftAboutReserveEngineer/Models/AppDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using DraftAboutReserveEngineer.Helpers;
+using Microsoft.Extensions.Configuration;
 
 namespace DraftAboutReserveEngineer.Models;
 
@@ -21,9 +23,19 @@ public partial class AppDbContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+    //        //=> optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=DraftAboutReverseEngineer;Trusted_Connection=True;TrustServerCertificate=True;");
+    //        => optionsBuilder.UseSqlServer(
+    //            "Your_Connnection_String",
+    //            options => options.EnableRetryOnFailure());
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=DraftAboutReverseEngineer;Trusted_Connection=True;TrustServerCertificate=True;");
+    {
+        var config = ConfigurationHelper.GetConfiguration();
+        var connectionString = config.GetConnectionString("DefaultConnection");
+
+        optionsBuilder.UseSqlServer(connectionString);
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
